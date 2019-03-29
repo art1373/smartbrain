f58dc761b1ac588fdf1ccddb1e995712bf199245
art1373 <48440021+art1373@users.noreply.github.com>
2019-03-29 16:17:01 +0430

    new app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
@@ -9,10 +8,6 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-
-const app = new Clarifai.App({
- apiKey: '3343b2b001a8430389ea3440051cf251'
-});
 
 const particlesOptions = {
   particles: {
@@ -48,9 +43,6 @@ class App extends Component {
 
   }
 
-  //componentDidMount(){
-    //fetch('http://localhost:3000/').then(response => response.json()).then(console.log)
-  //}
   loadUser = (data) =>{
   this.setState({user:{
         id:data.id,
@@ -85,15 +77,18 @@ class App extends Component {
 }
 
 onButtonSubmit = () => {
-  const host = window.location.hostname
   this.setState({imageUrl: this.state.input});
-    app.models
-    .predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
-    .then(response => {
-      if(response) {
-        fetch('http://'+host+':3001/image', {
+      fetch('https://damp-meadow-81110.herokuapp.com//imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+       if(response) {
+        fetch('https://damp-meadow-81110.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
